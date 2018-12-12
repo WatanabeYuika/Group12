dcaead52906c80d85cff74ea241a02d952055a82
kanda25 <45504566+kanda25@users.noreply.github.com>
2018-12-12 16:19:13 +0900

    Add files via upload

--- MapGameController.java
+++ MapGameController.java
@@ -9,80 +9,118 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.KeyCode;
 import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.shape.Rectangle;
+import javafx.animation.AnimationTimer;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javafx.application.Application;
 
 public class MapGameController implements Initializable {
-    public MapData mapData;
-    public MoveChara chara;
-    public GridPane mapGrid;
-    public ImageView[] mapImageViews;
-//    public Group[] mapGroups;
+	public MapData mapData;
+	public MoveChara chara;
+	public GridPane mapGrid;
+	public ImageView[] mapImageViews;
+	public mus mus;
+	//    public Group[] mapGroups;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        mapData = new MapData(21,15);
-        chara = new MoveChara(1,1,mapData);
-//        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
-        mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
-        for(int y=0; y<mapData.getHeight(); y++){
-            for(int x=0; x<mapData.getWidth(); x++){
-                int index = y*mapData.getWidth() + x;
-                mapImageViews[index] = mapData.getImageView(x,y);
-            }
-        }
-        mapPrint(chara, mapData);
-    }
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		mapData = new MapData(21,15);
+		chara = new MoveChara(1,1,mapData);
+		//        mapGroups = new Group[mapData.getHeight()*mapData.getWidth()];
+		mapImageViews = new ImageView[mapData.getHeight()*mapData.getWidth()];
+		for(int y=0; y<mapData.getHeight(); y++){
+			for(int x=0; x<mapData.getWidth(); x++){
+				int index = y*mapData.getWidth() + x;
+				mapImageViews[index] = mapData.getImageView(x,y);
+			}
+		}
+		mapPrint(chara, mapData);
+	}
 
-    public void mapPrint(MoveChara c, MapData m){
-        int cx = c.getPosX();
-        int cy = c.getPosY();
-        mapGrid.getChildren().clear();
-        for(int y=0; y<mapData.getHeight(); y++){
-            for(int x=0; x<mapData.getWidth(); x++){
-                int index = y*mapData.getWidth() + x;
-                if (x==cx && y==cy) {
-                    mapGrid.add(c.getCharaImageView(), x, y);
-                } else {
-                    mapGrid.add(mapImageViews[index], x, y);
-                }
-            }
-        }
-    }
+	public void mapPrint(MoveChara c, MapData m){
+		int cx = c.getPosX();
+		int cy = c.getPosY();
+		mapGrid.getChildren().clear();
+		for(int y=0; y<mapData.getHeight(); y++){
+			for(int x=0; x<mapData.getWidth(); x++){
+				int index = y*mapData.getWidth() + x;
+				if (x==cx && y==cy) {
+					mapGrid.add(c.getCharaImageView(), x, y);
+				} else {
+					mapGrid.add(mapImageViews[index], x, y);
+				}
+			}
+		}
+	}
 
-    public void func1ButtonAction(ActionEvent event) { }
-    public void func2ButtonAction(ActionEvent event) { }
-    public void func3ButtonAction(ActionEvent event) { }
-    public void func4ButtonAction(ActionEvent event) { }
+	public void func1ButtonAction(ActionEvent event) { }
+	public void func2ButtonAction(ActionEvent event) { }
+	public void func3ButtonAction(ActionEvent event) { }
+	public void func4ButtonAction(ActionEvent event) { }
 
-    public void keyAction(KeyEvent event){
-        KeyCode key = event.getCode();
-        if (key == KeyCode.DOWN){
-            downButtonAction();
-        }else if (key == KeyCode.RIGHT){
-            rightButtonAction();
-        }
-    }
+	public void keyAction(KeyEvent event){
+		KeyCode key = event.getCode();
+		if (key == KeyCode.DOWN){
+			downButtonAction();
+		}else if (key == KeyCode.RIGHT){
+			rightButtonAction();
+		}else if (key == KeyCode.LEFT){
+			leftButtonAction();
+		}else if (key == KeyCode.UP){
+			upButtonAction();
+		}
+	}
 
-    public void outputAction(String actionString) {
-        System.out.println("Select Action: " + actionString);
-    }
+	public void outputAction(String actionString) {
+		System.out.println("Select Action: " + actionString);
+	}
 
-    public void downButtonAction(){
-        outputAction("DOWN");
-        chara.setCharaDir(MoveChara.TYPE_DOWN);
-        chara.move(0, 1);
-        mapPrint(chara, mapData);
-    }
-    public void downButtonAction(ActionEvent event) {
-        downButtonAction();
-    }
-
-    public void rightButtonAction(){
-        outputAction("RIGHT");
-        chara.setCharaDir(MoveChara.TYPE_RIGHT);
-        chara.move( 1, 0);
-        mapPrint(chara, mapData);
-    }
-    public void rightButtonAction(ActionEvent event) {
-        rightButtonAction();
-    }
+	public void downButtonAction(){
+		outputAction("DOWN");
+		chara.setCharaDir(MoveChara.TYPE_DOWN);
+		chara.move(0, 1);
+		mapPrint(chara, mapData);
+	}
+	public void downButtonAction(ActionEvent event) {
+		downButtonAction();
+	}
+	public void leftButtonAction(){
+		outputAction("LEFT");
+		chara.setCharaDir(MoveChara.TYPE_LEFT);
+		chara.move(-1, 0);
+		mapPrint(chara, mapData);
+	}
+	public void leftButtonAction(ActionEvent event) {
+		leftButtonAction();
+	}
+	public void rightButtonAction(){
+		outputAction("RIGHT");
+		chara.setCharaDir(MoveChara.TYPE_RIGHT);
+		chara.move( 1, 0);
+		mapPrint(chara, mapData);
+	}
+	public void rightButtonAction(ActionEvent event) {
+		rightButtonAction();
+	}
+	public void upButtonAction(){
+		outputAction("UP");
+		chara.setCharaDir(MoveChara.TYPE_UP);
+		chara.move(0, -1);
+		mapPrint(chara, mapData);
+	}
+	public void upButtonAction(ActionEvent event) {
+		upButtonAction();
+	}  
 }
